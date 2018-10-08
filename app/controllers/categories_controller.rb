8a84d99baf8eb1a14209ec31aa8f38d4f2d12e0e
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-10-08 17:39:57 +0200

    path to cat subcat fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,12 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def index
+    @booktemplates = Booktemplate.all
     @categories = Category.all
   end
 
   def show
+    @booktemplates = Booktemplate.all
     @category = Category.find(params[:id])
     @subcategories = Subcategory.where(category_id: @category.id)
   end
--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -2,12 +2,14 @@ class SubcategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def index
+    @booktemplates = Booktemplate.all
     @subcategories = Subcategory.all
   end
 
   def show
+    @booktemplates = Booktemplate.all
     @subcategory = Subcategory.find(params[:id])
-    @booktemplates = Booktemplate.where(subcategory_id: @subcategory.id)
+    # @booktemplates = Booktemplate.where(subcategory_id: @subcategory.id)
   end
 
   def new
